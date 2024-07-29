6b332d0e58be32686a8150ecabef429cbb9bb70e
aleksGdz <aleksgaldamez2002@gmail.com>
2024-07-29 07:55:24 -0600

    Update

--- src/main/java/org/agaldamez/session/util/DBConnection.java
+++ src/main/java/org/agaldamez/session/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/test_java?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "javadev2002";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
