9075f3d584c2e5a1207d2316173ae01cea5fe2b9
Aleks Galdamez <105068825+gdzdev@users.noreply.github.com>
2025-01-14 17:00:53 -0600

    Actualizar DBConnection.java

--- src/main/java/org/agaldamez/session/util/DBConnection.java
+++ src/main/java/org/agaldamez/session/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/test_java?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/DB_NAME?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
